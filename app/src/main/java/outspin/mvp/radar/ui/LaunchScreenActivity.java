966f858a991437b93afc214e7d7efb143610c8ba
joidk <95649301+joidk@users.noreply.github.com>
2022-02-25 19:28:44 +0000

    PhoneNumber and Password Validation

--- app/src/main/java/outspin/mvp/radar/ui/LaunchScreenActivity.java
+++ app/src/main/java/outspin/mvp/radar/ui/LaunchScreenActivity.java
@@ -1,6 +1,8 @@
 package outspin.mvp.radar.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,23 @@ public class LaunchScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent launchIntent = new Intent(this, LoginActivity.class);
-        startActivity(launchIntent);
+        String sharedPrefsFile = "outspin.mvp.radar.PREFERENCE_FILE_TOKEN";
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                sharedPrefsFile, Context.MODE_PRIVATE);
 
-        finish();
+        String password = "";
+        String phoneNumber = "";
+        String validatedPassword = sharedPref.getString("password", password);
+        String validatedPhoneNumber = sharedPref.getString("phoneNumber", phoneNumber);
+
+        if(validatedPassword == "" || validatedPhoneNumber == "") {
+            Intent launchIntent = new Intent(this, LoginActivity.class);
+            startActivity(launchIntent);
+            finish();
+        } else{
+
+        }
+
+
     }
 }
--- app/src/main/java/outspin/mvp/radar/ui/LoginActivity.java
+++ app/src/main/java/outspin/mvp/radar/ui/LoginActivity.java
@@ -2,7 +2,11 @@ package outspin.mvp.radar.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import outspin.mvp.radar.R;
 import outspin.mvp.radar.databinding.ActivityLoginBinding;
@@ -17,5 +21,27 @@ public class LoginActivity extends AppCompatActivity {
 
         loginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(loginBinding.getRoot());
+
+        loginBinding.btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String password = loginBinding.etLoginPassword.getText().toString();
+                String phoneNumber = loginBinding.etLoginPhone.getText().toString();
+
+                String sharedPrefsFile = "outspin.mvp.radar.PREFERENCE_FILE_TOKEN";
+                SharedPreferences sharedPref = getApplication().getSharedPreferences(
+                        sharedPrefsFile, Context.MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("password", password);
+                editor.putString("phoneNumber", phoneNumber);
+                editor.apply();
+
+                Intent intent =  new Intent(LoginActivity.this, LaunchScreenActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
