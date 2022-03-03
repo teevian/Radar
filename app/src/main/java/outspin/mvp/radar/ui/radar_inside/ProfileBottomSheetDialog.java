9fd1b493657b10012c26dc50b498fd8438f9444d
joidk <95649301+joidk@users.noreply.github.com>
2022-03-03 17:08:23 +0000

    Complete dialog_interact_with_user.xml

--- app/src/main/java/outspin/mvp/radar/ui/radar_inside/ProfileBottomSheetDialog.java
+++ app/src/main/java/outspin/mvp/radar/ui/radar_inside/ProfileBottomSheetDialog.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +13,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.squareup.picasso.Picasso;
 
+import outspin.mvp.radar.R;
 import outspin.mvp.radar.databinding.DialogInteractWithUserBinding;
 import outspin.mvp.radar.models.User;
 
@@ -32,6 +34,11 @@ public class ProfileBottomSheetDialog extends BottomSheetDialogFragment  {
                 DialogInteractWithUserBinding.inflate(LayoutInflater.from(getContext()));
 
         dialog.setContentView(interactWithUserDialogbinding.getRoot());
+
+        android.view.ViewGroup.LayoutParams params = interactWithUserDialogbinding.dialogContainer.getLayoutParams();
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        params.height = (int) getResources().getDimension(R.dimen.dialog_interact_with_user_height);
+        interactWithUserDialogbinding.dialogContainer.setLayoutParams(params);
 
         Picasso.with(getContext())
                 .load(user.getPhotoURL())
